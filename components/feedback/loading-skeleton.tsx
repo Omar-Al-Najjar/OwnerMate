532659c0e0bac20b3fb328a6698d81bd3e1e6c9a
RetajMelhem <retajmelhem92004@gmail.com>
2026-03-21 18:09:40 +0300

    Delete components directory

--- components/feedback/loading-skeleton.tsx
+++
@@ -1,7 +0,0 @@
-﻿export function LoadingSkeleton() {
-  return (
-    <div className="animate-pulse rounded-xl border border-border bg-surface p-6 text-sm text-muted">
-      Loading...
-    </div>
-  );
-}
