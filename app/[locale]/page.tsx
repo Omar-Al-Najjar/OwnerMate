715e24c38c1f94b93486f272fdb5b972aee2bcaf
RetajMelhem <retajmelhem92004@gmail.com>
2026-03-22 03:51:26 +0300

    Delete app directory

--- app/[locale]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { resolveLocale } from "@/lib/i18n/config";
-import type { LocaleParams } from "@/types/i18n";
-
-export default async function LocaleIndexPage({ params }: LocaleParams) {
-  const { locale } = await params;
-  const safeLocale = resolveLocale(locale);
-  redirect(`/${safeLocale}/dashboard`);
-}
