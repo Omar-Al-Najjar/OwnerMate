56ea3275c21a1c644a5498cfb47adc2e8575f07e
RetajMelhem <retajmelhem92004@gmail.com>
2026-03-22 03:51:32 +0300

    Delete components directory

--- components/layout/page-container.tsx
+++
@@ -1,9 +0,0 @@
-﻿export function PageContainer({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <main className="mx-auto flex w-full max-w-7xl flex-col px-4 py-6 md:px-8 md:py-8">
-      {children}
-    </main>
-  );
-}
